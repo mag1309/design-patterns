c1c944e6441edd8012cf15bd94f233b1a29e0610
Manish Gupta <manish@respond.com>
2020-08-22 21:00:21 +0530

    First Issue

---
+++ src/main/java/com/pattern/creational/Singleton.java
@@ -0,0 +1,18 @@
+package com.pattern.creational;
+
+public class Singleton {
+
+	private static Singleton instance = null;
+	
+	private Singleton()
+	{
+		
+	}
+	
+	public static Singleton getInstance()
+	{
+		if(instance == null)
+			instance = new Singleton();
+		return instance;	
+	}
+}
